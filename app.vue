5c6e2a987edc82df772aa1ddf9682d000bde88de
koba1108 <paseli.koba@gmail.com>
2022-12-11 20:05:47 +0900

    add sqlite-wasm in nuxt3

--- app.vue
+++ app.vue
@@ -1,8 +1,44 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>App</h1>
+    <ul>
+      <li v-for="u in users">
+        <div>id: {{ u.id }}</div>
+        <div>name: {{ u.name }}</div>
+        <div>createdAt: {{ u.createdAt }}</div>
+        <div>updatedAt: {{ u.updatedAt }}</div>
+      </li>
+    </ul>
+    <button @click="fetchUsers">Fetch User From Sqlite</button>
   </div>
 </template>
 <script lang="ts" setup>
-import SQL from 'sql.js'
+import {useNuxtApp} from "#imports";
+
+const {$db} = useNuxtApp()
+
+interface User {
+  id: number
+  name: string
+  createdAt: string
+  updatedAt: string
+}
+
+const users = ref<User[]>([])
+
+const fetchUsers = () => {
+  const query = `SELECT * FROM users`;
+  $db.exec(query).map((row) => {
+    users.value = []
+    row.values.map((v) => {
+      users.value.push({
+        id: Number(v[0]),
+        name: `${v[1]}`,
+        createdAt: `${v[2]}`,
+        updatedAt: `${v[3]}`
+      });
+    });
+    return row;
+  });
+}
 </script>
---
+++ plugins/db.client.ts
@@ -0,0 +1,42 @@
+import initSqlJs, {Database} from "sql.js";
+
+export default defineNuxtPlugin(async () => {
+  const locateFile = (file: string) => `/wasm/${file}`
+  const SQL = await initSqlJs({locateFile});
+  const db = new SQL.Database();
+
+  // テーブル作成＆テストデータ投入
+  createUserTable(db);
+  createSeedUsers(db);
+
+  return {
+    provide: {
+      db
+    }
+  }
+})
+
+const createUserTable = (db: Database): Database => {
+  return db.run("CREATE TABLE users (id, name, createdAt, updatedAt);");
+}
+
+const createSeedUsers = (db: Database): Database => {
+  const now = new Date().toLocaleString();
+  const users = [
+    {id: 1, name: 'Alice', createdAt: now, updatedAt: now},
+    {id: 2, name: 'Bob', createdAt: now, updatedAt: now},
+    {id: 3, name: 'Carol', createdAt: now, updatedAt: now},
+  ];
+  users.forEach((user) => {
+    const query = 'INSERT INTO users VALUES ($id, $name, $createdAt, $updatedAt);';
+    db.run(query, {
+      $id: user.id,
+      $name: user.name,
+      $createdAt: user.createdAt,
+      $updatedAt: user.updatedAt,
+    });
+    console.log(`Inserted user: ${user.name}`);
+  });
+  console.log('Inserted seed users.');
+  return db;
+}
