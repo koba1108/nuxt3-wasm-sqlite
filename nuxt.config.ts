6108e78f4dbf2f5cf99759c97917b4e87f9d3e0c
koba1108 <paseli.koba@gmail.com>
2022-12-15 13:51:03 +0900

    add5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    cdnURL: 'https://koba1108.github.io',
     baseURL: '/nuxt3-wasm-sqlite/',
+    cdnURL: 'https://koba1108.github.io/nuxt3-wasm-sqlite/',
   }
 })
